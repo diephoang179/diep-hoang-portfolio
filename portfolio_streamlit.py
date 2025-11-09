1653a8b8c9e0ca5146cb12daf6d598d37e09fb3a
Diep Hoang <hdiep179@gmail.com>
2025-11-10 01:26:06 +0700

    Update portfolio_streamlit.py

--- portfolio_streamlit.py
+++ portfolio_streamlit.py
@@ -78,7 +78,7 @@ if choice == 'Delivery Analysis Project':
         """)
     st.image("process_steps.jpg", caption="Process steps", use_container_width=True)
     st.write("""
-        **4. Result:**
+        **4. Result:**\\
         You can check the project results here:
     """)
     st.markdown("**Link [data](https://docs.google.com/spreadsheets/d/1yLLasMDPSpGWDgLYwJQxUi5f2DTypsJgGdGc8iNd3ac/edit?gid=1389441584#gid=1389441584)**")
@@ -118,6 +118,7 @@ if choice == 'Delivery Analysis Project':
         
         Additionally, this project help me improve valuable hands-on experience in applying data analysis tools to real operational data, reinforcing practical skills in extracting insights and supporting data-driven decisions.
         """)
+
 
 
 
