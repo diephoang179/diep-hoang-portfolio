93986f86ebb402443a563a784713f395e56b8600
Diep Hoang <hdiep179@gmail.com>
2025-11-11 13:56:23 +0700

    Update portfolio_streamlit.py

--- portfolio_streamlit.py
+++ portfolio_streamlit.py
@@ -49,7 +49,7 @@ if choice == 'About Me':
         **Key features:**\\
         ✔️  Data processing and visualizing.\\
         ✔️  Data analyzing.\\
-        **Details:** Please learn more at **Data Analysis Project** page.\\
+        **Details:** Please learn more at **Delivery Analysis Project** page.\\
         Link [dashboard](https://lookerstudio.google.com/reporting/f9760e61-0bdc-43f6-9989-2942b6fa4f96)
 
         2️⃣ **Customer Segmentation**\\
@@ -80,7 +80,7 @@ if choice == 'Delivery Analysis Project':
     st.image("process_steps.jpg", caption="Process steps", use_container_width=True)
     st.write("""
         **4. Result:**\\
-        You can check the project results here:
+        You can check the project result here:
     """)
     st.markdown("**Link [data](https://docs.google.com/spreadsheets/d/1yLLasMDPSpGWDgLYwJQxUi5f2DTypsJgGdGc8iNd3ac/edit?gid=1389441584#gid=1389441584)**")
     st.markdown("**Link [dashboard](https://lookerstudio.google.com/reporting/f9760e61-0bdc-43f6-9989-2942b6fa4f96)**")
@@ -119,6 +119,7 @@ if choice == 'Delivery Analysis Project':
         
         Additionally, this project help me improve valuable hands-on experience in applying data analysis tools to real operational data, reinforcing practical skills in extracting insights and supporting data-driven decisions.
         """)
+
 
 
 
